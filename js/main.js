1e6d7e2b84cfd3fbe3792b86fbcb884e1bfc3081
Mmy2000 <yahianada005@gmail.com>
2024-04-16 14:26:57 +0200

    updating

--- js/main.js
+++ js/main.js
@@ -16,3 +16,25 @@ $('nav a').click(function(e){
     let sectionOffset = $(linkHref).offset().top
     $('body').animate({scrollTop:sectionOffset},500)
 })
+$(function(){
+    $('#loading').slideUp(500,function(){
+        $('#loading').remove()
+        $('body').css('overflow','auto')
+    })
+    let aboutOffset = $('#about').offset().top
+
+$(window).scroll(function(){
+    let scrollTop = $(window).scrollTop()
+    if (scrollTop > aboutOffset) {
+        $('nav').css('backgroundColor','#161028')
+    }else{
+        $('nav').css('backgroundColor','transparent')
+    }
+    console.log(aboutOffset);
+})
+$('nav a').click(function(e){
+    let linkHref = $(e.target).attr('href')
+    let sectionOffset = $(linkHref).offset().top
+    $('body').animate({scrollTop:sectionOffset},500)
+})
+})
